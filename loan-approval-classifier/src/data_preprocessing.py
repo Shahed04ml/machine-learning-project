e17e96d37605dbed30fcfc15dbc72cdd7abae3f7
shahd abubaker <167632272+Shahed04ml@users.noreply.github.com>
2026-08-06 14:46:54 +0300

    the workflow is run data_preprocessing.py after that train_models.py and then evaluation.py and last visualization py

--- loan-approval-classifier/src/data_preprocessing.py
+++ loan-approval-classifier/src/data_preprocessing.py
@@ -10,43 +10,24 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+def load_data():
+   loan_dataset = pd.read_csv('../data/loan_approval_dataset.csv')
+   loan_dataset.columns = loan_dataset.columns.str.strip()
+   return loan_dataset
+loan_dataset = load_data()
+loan_dataset=loan_dataset.drop("loan_id", axis=1)
 
-loan_dataset = pd.read_csv('../data/loan_approval_dataset.csv')
+loan_dataset.insert(loc=6, column='loan_to_income', 
+                     value=loan_dataset['loan_amount'] / loan_dataset['income_annum'])
 
-print("First few rows of the dataset:")
-print(loan_dataset.head())
-print("______________________________________")
-print("Number of missing values in each column:")
-print(loan_dataset.isnull().sum())
-print("______________________________________")
-print("Dataset information:")
-print(loan_dataset.info())
-print("______________________________________")
-print("Dataset description:")
-print(loan_dataset.describe())
-print("______________________________________")
-print("Number of duplicate rows:")
-print(loan_dataset.duplicated().sum())
-print("______________________________________")
-print("Column names:")
-print(loan_dataset.columns)
-print("______________________________________")
-print("the shape of the dataset:")
-print(loan_dataset.shape)
-print("______________________________________")
+loan_dataset=loan_dataset.drop(["loan_amount","income_annum"], axis=1)
 #splitting the dataset into features and target variable
 features = loan_dataset.iloc[:, :-1]
 target=loan_dataset.iloc[:, -1]
-print(features.head(5))
-print(target.head(5))
-print("______________________________________")
+
 #the features are divided into numerical and categorical features
-num_features = ['loan_id', ' no_of_dependents', ' loan_amount', ' cibil_score',' residential_assets_value', ' commercial_assets_value',
-       ' luxury_assets_value', ' bank_asset_value']
-cat_features = [' education',' self_employed']
-print("Numerical features:", num_features)
-print("Categorical features:", cat_features)
-print("______________________________________")
+num_features =  ["no_of_dependents","loan_term","cibil_score","residential_assets_value","commercial_assets_value","luxury_assets_value","bank_asset_value",'loan_to_income']
+cat_features =['education','self_employed']
 #turning categorical features into numerical features using one hot encoding and scaling the numerical features
 num_transformer=StandardScaler()
 cat_transformer = OneHotEncoder(handle_unknown='ignore')
@@ -55,10 +36,9 @@ preprocessor = ColumnTransformer([
     ('cat', cat_transformer, cat_features)
 ])
 
-print("________________________________________")
+
 #splitting the dataset into training and testing sets
 X=  features
-y= loan_dataset[' loan_status']
+y= loan_dataset['loan_status']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(f"Training set shape: {X_train.shape}")
-print(f"Testing set shape: {X_test.shape}")
+
--- loan-approval-classifier/src/visualization.py
+++ loan-approval-classifier/src/visualization.py
@@ -1,47 +1,43 @@
-
+from data_preprocessing import *
 from train_models import *
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#visulizion of the data
-features[" education"].value_counts().plot(kind='barh')
+from evaluation import *
+sns.countplot(data=loan_dataset, x="education", color="pink")
+plt.title("education distribution")
 plt.show()
 
-features[" self_employed"].value_counts().plot(kind='barh')
+sns.countplot(data=loan_dataset, x="self_employed", color="purple")
+plt.title("self_employed distribution")
 plt.show()
 
-sns.countplot(data=loan_dataset, x=" loan_status", color="red")
+sns.countplot(data=loan_dataset, x="loan_status", color="red")
 plt.title("loan_status distribution")
 plt.show()
-
-y_pred_RF = pipeline_rf.predict(X_test)
 
 cm = confusion_matrix(y_test, y_pred_RF)
 
 plt.figure(figsize=(6,5))
-sns.heatmap(
-    cm,
-    annot=True,
-    fmt="d",
-    cmap="Blues"
-)
-
-plt.title("Random Forest")
+sns.heatmap(cm,
+            annot=True,
+            fmt='d',
+            cmap='Blues',
+            xticklabels=['Approved','Rejected'],
+            yticklabels=['Approved','Rejected'])
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("random forest Confusion Matrix")
 plt.show()
 
-
-y_pred_log = pipeline_lr.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred_log)
+cm = confusion_matrix(y_test, y_pred_lr)
 
 plt.figure(figsize=(6,5))
-sns.heatmap(
-    cm,
-    annot=True,
-    fmt="d",
-    cmap="Greens"
-)
+sns.heatmap(cm,
+            annot=True,
+            fmt='d',
+            cmap='Blues',
+            xticklabels=['Approved','Rejected'],
+            yticklabels=['Approved','Rejected'])
 
-plt.title("Logistic Regression")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Logistic Regression Confusion Matrix")
 plt.show()
